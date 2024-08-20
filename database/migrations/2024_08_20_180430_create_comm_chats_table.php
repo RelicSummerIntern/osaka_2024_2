742dd1aab4a0912177f85dd873458d3fe4d507fa
みきみき <kmikimiki5454@gmail.com>
2024-08-20 18:32:50 +0900

    コミュニティチャットDBを作成

--- database/migrations/2024_08_20_180430_create_comm_chats_table.php
+++ database/migrations/2024_08_20_180430_create_comm_chats_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('comm_chats', function (Blueprint $table) {
             $table->id();
             $table->integer("comm_id");
+            $table->integer("user_id");
             $table->timestamps();
         });
     }
