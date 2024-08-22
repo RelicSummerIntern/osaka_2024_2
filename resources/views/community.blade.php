35261dba474157cb1091ede8879d124dbad1524a
みきみき <kmikimiki5454@gmail.com>
2024-08-22 14:57:34 +0900

    コミュニティ画面修正

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>コミュニティ{{ $comm_name }}</title>
+    <title>コミュニティ：{{ $comm_name }}</title>
     <link rel="stylesheet" href="{{ asset('css/mypage.css') }}">
     <style>
         body {
