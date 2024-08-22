4a04515bca2a3e4d1ea568611d18a8ead9870309
みきみき <kmikimiki5454@gmail.com>
2024-08-22 10:48:37 +0900

    ログイン機能実装中

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,15 +40,15 @@
                 町名番地：<input type="text" class="p-street-address p-extended-address" />
          </form>
         <!-- Confirm Password -->
-        <!-- <div class="mt-4">
-             <label class>hoge</label>
+         <div class="mt-4">
+             <label class>confirm password</label>
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div> -->
+        </div> 
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
@@ -56,7 +56,7 @@
             </a>
 
             <x-primary-button class="ml-4">
-            <a class href="{{ route('register')}}">
+            <a class href="{{ route('register.post')}}">
                 {{ __('Register') }}
             </x-primary-button>
         </div>
